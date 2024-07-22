266120cf27b4deb715c4a6d8235f74f0b7690098
Antonio Jurić <juricantonio18@gmail.com>
2024-07-22 17:23:29 +0200

    Added title service for working with srt files

--- src/app/services/title.service.ts
+++ src/app/services/title.service.ts
@@ -1,6 +1,41 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
+import { map } from "rxjs";
+
+export interface Title{
+  id: number;
+  start: string;
+  end: string;
+  text: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class TitleService {}
+export class TitleService {
+  constructor(private http: HttpClient){}
+
+  loadTitle(title: string){
+    return this.http.get(title, {responseType: "text"}).pipe(
+      map(data => this.parseSRT(data))
+    )
+  }
+
+  parseSRT(data: string){
+    const blocks = data.trim().split("\n\n");
+    const titles: Title[] = [];
+
+    blocks.forEach(block => {
+      const lines = block.split("\n");
+      if(lines.length >= 3){
+        const id = parseInt(lines[0], 10);
+        const [start, end] = lines[1].split(" --> ");
+        const text = lines.slice(2).join(" ");
+
+        titles.push({id, start, end, text});
+      }
+    });
+
+    return titles;
+  }
+}
